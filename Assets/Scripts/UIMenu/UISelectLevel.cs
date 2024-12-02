4add788d3741d72b0e585e66e78d5236c0505e9d
Vlad <tomhanter785@gmail.com>
2024-12-02 14:48:20 +0300

    UIMenu-selectLevel

---
+++ Assets/Scripts/UIMenu/UISelectLevel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UISelectLevel : MonoBehaviour
+{
+    [SerializeField] private GameObject buttonPrefab;
+    [SerializeField] private GameObject levelMenu;
+    [SerializeField] private Transform buttonContent;
+    [SerializeField] private int maxLevels = 1000;
+    private Text buttonText;
+
+    private void Start()
+    {
+        GenerateButtons();
+    }
+
+    private void GenerateButtons()
+    {
+        for (int i = 1; i <= maxLevels; i++)
+        {
+            GameObject newButton = Instantiate(buttonPrefab, buttonContent);
+            buttonText = newButton.GetComponentInChildren<Text>();
+            buttonText.text = i.ToString();
+
+            int levelIndex = i;
+            newButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                PlayerPrefs.SetInt("level", levelIndex - 1);
+                LoadLelev();
+            });
+        }
+    }
+
+    private void LoadLelev()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ActiveMenu()
+    {
+        levelMenu.SetActive(true);
+    }
+}
